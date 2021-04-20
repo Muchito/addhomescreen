0bd37492e12f39c48b946d6d9d35f6e25fc0b05a
Muchito <76431724+Muchito@users.noreply.github.com>
2021-04-20 14:19:53 +0200

    update

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/fox1.jpg',
-      '/pwa-examples/a2hs/images/fox2.jpg',
-      '/pwa-examples/a2hs/images/fox3.jpg',
-      '/pwa-examples/a2hs/images/fox4.jpg',
+      '/addhomescreenss/a2hs/',
+      '/addhomescreenss/a2hs/index.html',
+      '/addhomescreenss/a2hs/index.js',
+      '/addhomescreenss/a2hs/style.css',
+      '/addhomescreenss/a2hs/images/fox1.jpg',
+      '/addhomescreenss/a2hs/images/fox2.jpg',
+      '/addhomescreenss/a2hs/images/fox3.jpg',
+      '/addhomescreenss/a2hs/images/fox4.jpg',
     ])),
   );
 });
